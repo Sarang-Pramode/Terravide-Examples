89d7db17cb35b33b77c751eb5b9b0e1339640316
Sarang-Pramode <sarangpramode1997@gmail.com>
2023-01-25 04:59:04 -0500

    created logfile and tested writing logs to it

--- Process_lasFiles.py
+++ Process_lasFiles.py
@@ -6,27 +6,43 @@ import src.modules.MultipleReturnsClassification as MRC
 
 from multiprocessing import Pool
 import logging
+import os
 
-logging.basicConfig(filename='Process.log', encoding='utf-8', level=logging.DEBUG)
+def InitiateLogger():
+    logfilename = 'ProcessLas.log' 
+    logger = logging.getLogger()
+    fhandler = logging.FileHandler(filename=logfilename, mode='a')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fhandler.setFormatter(formatter)
+    logger.addHandler(fhandler)
+    logger.setLevel(logging.INFO)
+
 
 def ProcessLas():
 
     try:
-        print('code here')
+        f = open("demofile.txt")
+        f.write('test')
     except Exception as e:
-        logging.error("Error Occured : ",e)
+        logging.error("Error Occured : "+str(e))
+        pass
 
 
 if __name__ == '__main__':
 
+    InitiateLogger()
+
     # Get Year Input from User
-    year = int(input("Enter Data Year to Process [2017 and 2021 supported] : "))
+    year = 2017 #int(input("Enter Data Year to Process [2017 and 2021 supported] : "))
     # Get List of filnames on FTP server
     filenames = util.FTP_GetFileList(year)
     # Prepare arguments
     args = []
 
     logging.info('testing logging module')
+    ProcessLas()
+
+    logging.info('testing logging module line 2')
 
     # with Pool(10) as p:
 
